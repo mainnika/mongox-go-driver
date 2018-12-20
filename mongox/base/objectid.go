b4d4fc666ecea727e280643e540cb68199f0eb38
Nikita Tokarchuk <tokarchuk+bitbucket@nevisq.com>
2018-12-20 01:49:33 +0100

    ObjectId base might be empty

--- mongox/base/objectid.go
+++ mongox/base/objectid.go
@@ -1,12 +1,15 @@
 package base
 
 import (
+	"github.com/mainnika/mongox-go-driver/mongox"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
 )
 
+var _ mongox.BaseObjectID = &ObjectID{}
+
 // ObjectID is a structure with objectId as an _id field
 type ObjectID struct {
-	ID primitive.ObjectID `bson:"_id" json:"_id"`
+	ID primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 }
 
 // GetID returns an _id
--- mongox/common.go
+++ mongox/common.go
@@ -1,5 +1,9 @@
 package mongox
 
+import (
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
+)
+
 type Saver interface {
 	Save(db *Database) error
 }
@@ -10,4 +14,9 @@ type Deleter interface {
 
 type Loader interface {
 	Load(db *Database, filters ...interface{}) error
+}
+
+type BaseObjectID interface {
+	GetID() primitive.ObjectID
+	SetID(id primitive.ObjectID)
 }
